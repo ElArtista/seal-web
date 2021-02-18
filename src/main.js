dd0db692e84f566c9c6a90b003dd9dfe47aacaca
Agorgianitis Loukas <agorglouk@gmail.com>
2021-02-18 16:46:51 +0200

    Set html, body and app size to 100%

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import 'normalize.css'
 import 'tailwindcss/tailwind.css'
+import '@/assets/css/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
