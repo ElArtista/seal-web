ceb5a870a057b36b3417752302e592c9e5bf3b42
Agorgianitis Loukas <agorglouk@gmail.com>
2021-02-18 16:57:58 +0200

    Remove generated boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full h-full flex justify-center items-center bg-gray-100">
+    <h1 class="text-2xl">Hello</h1>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
